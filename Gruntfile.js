74a4cf3f289647c6dc4ec97d375216331fcb33d4
Salvatore Tuscolano <s.tuscolano@bitmama.it>
2015-11-25 17:53:11 +0100

    all

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+ 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-wiredep');
+   
+  grunt.initConfig({
+       
+    sass: { 
+      dev: { 
+        files: {
+            'app/css/style.css': 'scss/style.scss'
+        }
+      }
+    },
+     
+    watch: {
+      sass: {
+        files: 'app/scss/*.scss',
+        tasks: ['sass']
+      },
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      }
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+            src : [
+              'app/css/*.css',
+              'app/*.html'
+            ]
+        },
+        options: {
+            watchTask: true,
+            server: './app',
+            https: false,
+            browser: ["google chrome"]
+        }
+      },
+    },
+    wiredep: {
+
+      task: {
+
+        src: [
+          'app/**/*.html',   // .html support...
+          'app/scss/style.scss',  // .scss & .sass support...
+        ],
+
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
+      }
+    }
+  });
+   
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
