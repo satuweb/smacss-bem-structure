70f252090f17ee6773811dfaec10e5c3f507e806
Salvatore Tuscolano <s.tuscolano@bitmama.it>
2015-12-14 18:02:40 +0100

    define materialize blocks

--- app/js/custom/load-partials.js
+++ app/js/custom/load-partials.js
@@ -5,7 +5,8 @@ $( document ).ready(function() {
     // LOAD PARTIALS
     $.get("../partials/menu-primary.html", '', function (data) { $(".menu-primary").append(data); });
     $.get("../partials/search-box.html", '', function (data) { $(".search-form").append(data); });
-    $.get("../partials/carousel.html", '', function (data) { $(".carousel").append(data); });
+    $.get("../partials/slider.html", '', function (data) { $(".carousel").append(data); });
+    $.get("../partials/parallax.html", '', function (data) { $(".parallax-container").append(data); });
     $.get("../partials/contact-form.html", '', function (data) { $(".contact").append(data); });
 
   $.ajaxSetup({ async: true });
--- app/js/custom/script.js
+++ app/js/custom/script.js
@@ -1,4 +1,12 @@
 //MAIN JAVASCRIPT
 $(document).ready(function(){
-  //TODO
+  $('.slider').slider({
+    full_width: true,
+    interval:5000,
+    transition:800,
+    indicators: true,
+    height: '100%'
+  });
+  $('.parallax').parallax();
+  $('textarea').characterCounter();
 });
