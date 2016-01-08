09bcd3bc23a3e72063a7fc53da6fa6ac00ab2cf9
Salvatore Tuscolano <s.tuscolano@bitmama.it>
2016-01-08 17:39:39 +0100

    change framework grid

--- app/js/custom/script.js
+++ app/js/custom/script.js
@@ -1,12 +1,4 @@
 //MAIN JAVASCRIPT
 $(document).ready(function(){
-  $('.slider').slider({
-    full_width: true,
-    interval:5000,
-    transition:800,
-    indicators: true,
-    height: '100%'
-  });
-  $('.parallax').parallax();
-  $('textarea').characterCounter();
+ //TODO
 });
